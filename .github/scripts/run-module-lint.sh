0c3cffc9cc04d2d486e571b5a5719eb6cdddeb3e
Elliot Winkler <elliot.winkler@gmail.com>
2024-06-20 12:08:02 -0600

    Fix

---
+++ .github/scripts/run-module-lint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [[ -z "$PROJECT_NAME" ]]; then
+  echo "Missing PROJECT_NAME."
+  exit 1
+fi
+
+yarn run-tool "$PROJECT_NAME" > /tmp/module-lint-report.txt
+
+exitcode=$?
+
+cat /tmp/module-lint-report.txt
+
+if [[ $exitcode -eq 0 ]]; then
+  exit 0
+elif [[ $exitcode -ne -127 ]]; then
+  exit 1
+fi
